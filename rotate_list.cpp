59b20467f4400983a2ec7577abc14e185db14244
Deepak <128701418+deepak61296@users.noreply.github.com>
2025-02-14 20:30:49 +0530

    Create rotate_list.cpp

---
+++ rotate_list.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (!head) return nullptr;
+        int n = 1;
+        ListNode* temp = head;
+        while (temp->next) {
+            n++;
+            temp = temp->next;
+        }
+        k %= n;
+        temp->next = head;
+        for (int i = 0; i < n - k; i++) temp = temp->next;
+        ListNode* new_head = temp->next;
+        temp->next = nullptr;
+        return new_head;
+    }
+};
