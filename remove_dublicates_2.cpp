93f6986d6e5ce274d0a12de7b531b698f198e337
Deepak <128701418+deepak61296@users.noreply.github.com>
2025-02-14 20:27:58 +0530

    Update remove_dublicates_2.cpp

--- remove_dublicates_2.cpp
+++ remove_dublicates_2.cpp
@@ -1,43 +1,18 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head==null || head.next ==null){
-            return head;
-        }
-        ListNode dummy = new ListNode(0,head);
-        ListNode prev = dummy;
-        ListNode curr = head;
-        ListNode temp = head.next;
-        boolean flag = false;
-
-        while(temp != null){
-            if(curr.val != temp.val){
-                if(flag == true){
-                    prev.next = temp;
-                    flag = false;
-                }
-                else{
-                    prev = prev.next;
-                }
-            }
-            else if(curr.val == temp.val){
-                flag = true;
-            }
-            temp = temp.next;
-            curr = curr.next;
-        }
-        if(flag == true){
-            prev.next = temp;
-        }
-        return dummy.next;
+ public:
+  ListNode* deleteDuplicates(ListNode* head) {
+    ListNode dummy(0, head);
+    ListNode* prev = &dummy;
+    while (head) {
+      while (head->next && head->val == head->next->val)
+        head = head->next;
+      if (prev->next == head)
+        prev = prev->next;
+      else
+        prev->next = head->next;
+      head = head->next;
     }
-}
+
+    return dummy.next;
+  }
+};
